1c1a10a95bdfbb05f2c240fc56d292f6a1ebc7f3
Salakar <mike.diarmid@gmail.com>
2019-04-19 19:38:36 +0100

    🔥

---
+++ template.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: "HelloWorld",
+
+  // Directory with template
+  templateDir: "./project",
+
+  // Path to script, which will be executed after init
+  postInitScript: "./post-init.js"
+};
